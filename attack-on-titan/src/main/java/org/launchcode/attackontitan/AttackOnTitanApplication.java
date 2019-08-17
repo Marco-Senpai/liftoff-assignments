ed917239f0eb23ba2c70b11c3a3ad6efed8d7ca1
Marco-Senpai <mcneilmarcus92@gmail.com>
2019-08-17 15:54:35 -0500

    p4

---
+++ attack-on-titan/src/main/java/org/launchcode/attackontitan/AttackOnTitanApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.attackontitan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttackOnTitanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttackOnTitanApplication.class, args);
+	}
+
+}
